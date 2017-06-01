cd5c2df2c81fb513c9c08ea2b9f8047134350161
Ahmad Zakaria <ahmadz1991@gmail.com>
2017-06-01 16:09:03 +0200

    handled qt error

--- keras-tf-py3/run.sh
+++ keras-tf-py3/run.sh
@@ -1,11 +1,15 @@
 # nvidia-docker run --privileged -it --rm --name keras cuda/keras
 xhost +local:root
-nvidia-docker run --privileged --rm
-              --net=host
-              -p 8888:8888
-              -it
-              -v $HOME/Documents:/root/Documents
-              --env KERAS_BACKEND=tensorflow
+nvidia-docker run --privileged \
+              -p 8888:8888 \
+              -it \
+              --net=host \
+	            -e DISPLAY \
+	            -e USER \
+              -e "http_proxy=$http_proxy"  \
+              -e "https_proxy=$https_proxy"  \
+              -v $HOME/Documents:/root/Documents \
+              --env KERAS_BACKEND=tensorflow \
               zack/keras:tensorflow-py3 /bin/bash
 # xhost +local:root
 # nvidia-docker run --privileged -it \
