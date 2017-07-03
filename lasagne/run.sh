15c211046768528364b2d5530b84e5ad2db40ee4
Ahmad Zakaria <ahmadz1991@gmail.com>
2017-07-03 21:38:23 +0200

    adding lasagne

---
+++ lasagne/run.sh
@@ -0,0 +1,24 @@
+# nvidia-docker run --privileged -it --rm --name keras cuda/keras
+xhost +local:root
+nvidia-docker run --privileged \
+              -p 8888:8888 \
+              -it \
+              --net=host \
+	            -e DISPLAY \
+	            -e USER \
+              -e "http_proxy=$http_proxy"  \
+              -e "https_proxy=$https_proxy"  \
+              -v $HOME:/home/ahmad \
+              zack/lasagne /bin/bash
+# xhost +local:root
+# nvidia-docker run --privileged -it \
+#                     -v $HOME/Documents:/root/Documents \
+#                     -v $HOME/Documents/DockerCache/16.04:/var/cache/apt/archives/ \
+#                     -e "http_proxy=$http_proxy"  \
+#                     -e "https_proxy=$https_proxy"  \
+#                     -e "ftp_proxy=$ftp_proxy" \
+#                     --name="zack-cv2_base" \
+#                     -e DISPLAY \
+#                     --net=host  \
+#                     opencv2 /bin/bash
+xhost -local:root
